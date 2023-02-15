df8f163fa10509a5b664fa0517bb80dd22b2b08e
Clementine Amar <clementine_cohen@hotmail.com>
2023-02-15 19:54:15 +0200

    first submission

--- js/service/memes.service.js
+++ js/service/memes.service.js
@@ -8,7 +8,23 @@ var gCurrMeme
 var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
 
 var gImgs = [{ id: 1, url: '/images/1.jpg', keywords: ['funny', 'cat'] },
-{ id: 2, url: '/images/2.jpg', keywords: ['dog', 'cute'] }];
+{ id: 2, url: '/images/2.jpg', keywords: ['dog', 'cute'] },
+{id:3, url: '/images/3.jpg', keywords: ['dog', 'baby', 'cute', 'sleep']},
+{id:4, url: '/images/4.jpg', keywords: ['cat', 'sleep']},
+{id:5, url: '/images/5.jpg', keywords: ['baby']},
+{id:6, url: '/images/6.jpg', keywords: ['funny']},
+{id:7, url: '/images/7.jpg', keywords: ['baby', 'funny']},
+{id:8, url: '/images/8.jpg', keywords: ['funny', 'happy']},
+{id:9, url: '/images/9.jpg', keywords: ['baby', 'funny']},
+{id:10, url: '/images/10.jpg', keywords: ['funny', 'happy']},
+{id:11, url: '/images/11.jpg', keywords: ['happy']},
+{id:12, url: '/images/12.jpg', keywords: ['funny']},
+{id:13, url: '/images/13.jpg', keywords: ['happy']},
+{id:14, url: '/images/14.jpg', keywords: ['angry', 'glasses']},
+{id:15, url: '/images/15.jpg', keywords: ['angry']},
+{id:16, url: '/images/16.jpg', keywords: ['funny']},
+{id:17, url: '/images/17.jpg', keywords: ['funny', 'two']},
+{id:18, url: '/images/18.jpg', keywords: ['future', 'funny']},];
 
 var gMeme = {
     selectedImgId: 1,
@@ -26,7 +42,7 @@ function getMeme(){
     return gMeme
 }
 
-renderMemes(){
+function renderMemes(){
     // renders meme to editor with all infos in meme
 }
 
